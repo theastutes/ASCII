7e45ae62b2440d9798be80f7a08ea83ca6b31e8f
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-19 22:03:18 +0530

    updated cors

--- node_server/index.js
+++ node_server/index.js
@@ -25,11 +25,7 @@ main().catch(err => console.log(err));
 app.use(bodyParser.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
-app.use(cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-}));
+app.use(cors());
 
 app.use("/api/users",userRouter);
 app.use("/api/posts",postRouter);
