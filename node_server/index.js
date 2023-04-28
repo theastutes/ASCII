f686034a659bd6efcb895098a5e05ce625f987c7
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-28 12:10:25 +0530

    Update index.js

--- node_server/index.js
+++ node_server/index.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 app.use(cors({
-    origin: ["*"],
+    origin: [process.env.FRONTEND_URL,"http://localhost:4000",],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
 }));
