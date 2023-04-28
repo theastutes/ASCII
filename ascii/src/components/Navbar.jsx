c29352968dc8238e70303b56531c6bb7ae1f0d87
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-28 15:11:25 +0530

    update focus color

--- ascii/src/components/Navbar.jsx
+++ ascii/src/components/Navbar.jsx
@@ -6,23 +6,23 @@ export default function Navbar() {
   const user = store.get("user");
   return (
     <div className="fixed w-screen rounded-lg navbar  z-50 flex justify-around bg-white/40 backdrop-blur-lg bottom-[1px] pt-2 pb-2 text-xl shadow-2xl shadow-black">
-      <Link to={user ? `/dashboard` : ""} className=" text-xl focus:text-seagreen  text-black">
+      <Link to={user ? `/dashboard` : ""} className=" text-xl  text-zinc-900">
         <button>
           <i class="fa fa-home" aria-hidden="true"></i>
         </button>
       </Link>
-      <Link to={user ? `/quizop` : ""} className="text-xl focus:text-seagreen text-black">
+      <Link to={user ? `/quizop` : ""} className="text-xl  text-zinc-900">
         <button>
           {" "}
           <i class="fa fa-question-circle" aria-hidden="true"></i>
         </button>
       </Link>
       <button>
-        <Link to={user ? `/leaderboard` : ""} className="text-xl focus:text-seagreen text-black">
+        <Link to={user ? `/leaderboard` : ""} className="text-xl  text-zinc-900">
           <i class="fa fa-line-chart" aria-hidden="true"></i>
         </Link>
       </button>
-      <div className="text-xl focus:text-seagreen text-black">
+      <div className="text-xl  text-zinc-900">
       <Link to={user?`/studymaterial`:""}>
         <i class="fa fa-book" aria-hidden="true"></i>
       
