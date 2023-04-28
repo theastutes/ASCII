590a4c905ab3bd2391afaf6494f166731dfda06d
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-28 10:29:38 +0530

    Uploading new folder

--- node_server/routes/quizroutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import {getquiz,quizform} from '../controllers/quizmodule.js'
-
-const router = express.Router();
-
-router.post('/quizform',quizform);
-router.get('/getquiz',getquiz);
-
-export default router;
