e9b9bf8aeef6a8f07d36524d5fcc18524b151d4b
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-29 19:52:52 +0530

    ......

--- ascii/src/components/WelcomeBack.jsx
+++ ascii/src/components/WelcomeBack.jsx
@@ -12,11 +12,11 @@ export default function WelcomeBack({setwpage}){
     },[]);
     return (
         <div className='absolute w-screen mt-10 fd:ml-24 h-screen m-2 rounded-xl flex flex-col text-white items-center backdrop-blur-sm'>
-            <h1 className='md:text-4xl text-5xl  mt-14 m-8 text-zinc-900 '> Hey! {usernm}</h1>
-            <p className='text-zinc-800 mt-4  '>Check out latest updates now!</p>
+            <h1 className='md:text-4xl text-5xl  mt-14 m-8 text-zinc-900 mb-14'> Hey! {usernm}</h1>
+            <p className='text-zinc-800 m-4  mt-8'>Check out latest updates now!</p>
             
             <Link to = "/dashboard" onClick = {setwpage}>
-                <button className='loginbtn text-lg m-14 '>Go to Dashboard</button>
+                <button className='loginbtn text-lg m-8 '>Go to Dashboard</button>
             </Link>
 
         </div>
