3c80cb296cd8503e0de16febef5cae10f54d6816
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-29 19:41:09 +0530

    I hope it'll work

--- ascii/src/components/WelcomeBack.jsx
+++ ascii/src/components/WelcomeBack.jsx
@@ -11,12 +11,12 @@ export default function WelcomeBack({setwpage}){
         setUsernm(name);
     },[]);
     return (
-        <div className='flex flex-col justify-center items-center'>
-            <h1 className='mb-5'> Hey! {usernm}</h1>
-            <p className='m-5'>Check out latest updates now!</p>
+        <div className='w-screen mt-10 fd:ml-24 h-screen m-2 rounded-xl z-10 flex flex-col text-white items-center backdrop-blur-sm'>
+            <h1 className='md:text-4xl text-5xl  mt-14 m-8 text-zinc-900 z-10'> Hey! {usernm}</h1>
+            <p className='text-zinc-800 w-3/4 m-4 ml-24 z-10'>Check out latest updates now!</p>
             
             <Link to = "/dashboard" onClick = {setwpage}>
-                <button className='loginbtn'>Go to Dashboard</button>
+                <button className='loginbtn text-lg m-14 z-10'>Go to Dashboard</button>
             </Link>
 
         </div>
