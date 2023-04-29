e9de02b2dead7d8a1daf114f7ceed54db04d1c38
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-29 22:20:56 +0530

    testing......

--- ascii/src/components/WelcomeBack.jsx
+++ ascii/src/components/WelcomeBack.jsx
@@ -4,28 +4,9 @@ import { Link } from "react-router-dom";
 
 export default function WelcomeBack({setwpage}){
 
-    const [usernm,setUsernm] = useState('');
-    useEffect(()=>{
-       const mypromise = new Promise((resolve,reject) =>{
-            data = store.get('user');
-            if(data){
-                if(data){
-                    const usr = data.name;
-                    setUsernm(usr);
-                    resolve();
-                }else{
-                    reject(setUsernm(""));
-                }
-            }
-        });
-        mypromise.then(()=>{
-            setwpage(true);
-
-        })
-    },[]);
     return (
         <div className='absolute w-screen mt-10 fd:ml-24 h-screen m-2 rounded-xl flex flex-col text-white items-center backdrop-blur-sm'>
-            <h1 className='md:text-4xl text-5xl  mt-14 m-8 text-zinc-900 mb-14'> Hey! {usernm}</h1>
+            <h1 className='md:text-4xl text-5xl  mt-14 m-8 text-zinc-900 mb-14'> Hey There!</h1>
             <p className='text-zinc-800 m-4  mt-8'>Check out latest updates now!</p>
             
             <Link to = "/dashboard" onClick = {setwpage}>
