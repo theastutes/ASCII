db57627714c36d2e1fc8865b7f8297b3f7db97b9
Maurya-Karan <karanmaurya81002@gmail.com>
2023-04-29 19:43:09 +0530

    I have nothing to say

--- ascii/src/components/WelcomeBack.jsx
+++ ascii/src/components/WelcomeBack.jsx
@@ -11,7 +11,7 @@ export default function WelcomeBack({setwpage}){
         setUsernm(name);
     },[]);
     return (
-        <div className='w-screen mt-10 fd:ml-24 h-screen m-2 rounded-xl z-10 flex flex-col text-white items-center backdrop-blur-sm'>
+        <div className='absolute w-screen mt-10 fd:ml-24 h-screen m-2 rounded-xl z-10 flex flex-col text-white items-center backdrop-blur-sm'>
             <h1 className='md:text-4xl text-5xl  mt-14 m-8 text-zinc-900 z-10'> Hey! {usernm}</h1>
             <p className='text-zinc-800 w-3/4 m-4 ml-24 z-10'>Check out latest updates now!</p>
             
